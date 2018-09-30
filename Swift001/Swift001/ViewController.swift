f3cf34054fd58d87745fa785a1a814a0962655c6
Lethe <976244150@qq.com>
2018-09-30 09:30:07 +0800

    no message

--- Swift001/Swift001/ViewController.swift
+++ Swift001/Swift001/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let maxNum = 100
+    var changeNum = 0
+    var testString: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let count = makeCake(name: "lihuan", count: 5)
+        assert(count >= 5,"A cake large than 4")
+        
+        makeRange();
+        
     }
 
+    func makeCake(name:String,count:Int) -> Int {
+        if name == "lihuan" {
+            return count
+        }
+        return 0
+    }
+    
+    func makeRange() {
+        let names = ["li","zhang","wang","he"]
+        
+        
+        for index in names[..<2] {
+            print("test name \(index)")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
